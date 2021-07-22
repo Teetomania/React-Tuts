ff335847c25e6df2b43089d274f681f6b49c012c
Tomisin Owolabi <48669572+Teetomania@users.noreply.github.com>
2021-07-22 09:30:11 +0100

    Using useeffects, usestate and hooks

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,25 @@
+import { useState, useEffect } from 'react';
+import Bloglist from './Bloglist';
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
+    const [name, setName] = useState('mario');
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
+    useEffect(() => {
+        console.log('use effect ran');
+    },[name])
     return ( 
         <div className="home">
-            <h2>Home page</h2>
+            <Bloglist blogs={blogs} title = "All blogs title!" handleDelete= {handleDelete}/>
+            <button onClick= {() => setName('luigi')}>change name</button>
+            <p>{ name }</p>
         </div>
      );
 }
